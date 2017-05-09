3017e7ca04c5033e54353f8fb79ed442a85b0e54
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-05-09 18:53:56 -0400

    completed github.com/EladBezalel/material2-start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import {MdIconRegistry, MdDialog, MdDialogRef} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
+import {DialogComponent} from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    isDarkTheme = false;
+    users = [
+    {
+      name: 'Lia Lugo',
+      avatar: 'svg-11',
+      details: 'I love cheese, ...',
+      isAdmin: true,
+      isCool: false
+    },
+    {
+      name: 'George Duke',
+      avatar: 'svg-12',
+      details: 'Zombie ipsum ...',
+      isAdmin: false,
+      isCool: true
+    }
+  ];
+
+  selectedUser = this.users[0];
+
+	constructor(iconRegistry: MdIconRegistry, sanitizer: DomSanitizer, private dialog: MdDialog) {
+    // To avoid XSS attacks, the URL needs to be trusted from inside of your application.
+    const avatarsSafeUrl = sanitizer.bypassSecurityTrustResourceUrl('./assets/avatars.svg');
+
+    iconRegistry.addSvgIconSetInNamespace('avatars', avatarsSafeUrl);
+  }
+
+  private openAdminDialog() {
+		this.dialog.open(DialogComponent).afterClosed()
+		.filter(result => !!result)
+		.subscribe(user => {
+			this.users.push(user);
+			this.selectedUser = user;
+		});
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {MaterialModule} from '@angular/material';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+  AppComponent,
+  DialogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     FlexLayoutModule,
+    BrowserAnimationsModule,
     MaterialModule.forRoot()
   ],
   providers: [],
+  entryComponents: [DialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
