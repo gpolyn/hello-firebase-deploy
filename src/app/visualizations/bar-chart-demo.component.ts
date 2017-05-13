673f521dae04a7fe278e68ed5999c66834b716f4
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-05-13 17:09:00 -0400

    bar chart swipeable

--- src/app/visualizations/bar-chart-demo.component.ts
+++ src/app/visualizations/bar-chart-demo.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bar-chart-demo',
+	host: {class: 'myClass'},
+  styleUrls: ['./visualization.component.css'],
   templateUrl: './bar-chart-demo.html'
 })
 export class BarChartDemoComponent {
@@ -14,9 +16,36 @@ export class BarChartDemoComponent {
   public barChartLegend:boolean = true;
 
   public barChartData:any[] = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', visible: true},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', visible: false},
+    {data: [18, 41, 80, 69, 80, 47, 50], label: 'Series C', visible: false}
   ];
+
+  public currentBarChartData = [this.barChartData[0]];
+  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+
+  swipe(currentIndex: number, action: string = this.SWIPE_ACTION.RIGHT) {
+    console.log('swipe', currentIndex)
+    if (currentIndex > this.barChartData.length || currentIndex < 0) return;
+
+    let nextIndex = 0;
+    
+    // next
+    if (action === this.SWIPE_ACTION.RIGHT) {
+        const isLast = currentIndex === this.barChartData.length - 1;
+        nextIndex = isLast ? 0 : currentIndex + 1;
+    }
+
+    // previous
+    if (action === this.SWIPE_ACTION.LEFT) {
+        const isFirst = currentIndex === 0;
+        nextIndex = isFirst ? this.barChartData.length - 1 : currentIndex - 1;
+    }
+
+    // toggle avatar visibility
+    this.barChartData.forEach((x, i) => x.visible = (i === nextIndex));
+    this.currentBarChartData = [this.barChartData[nextIndex]];
+  }
 
   // events
   public chartClicked(e:any):void {
