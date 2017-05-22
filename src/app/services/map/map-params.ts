2004ad58f4f5dc0e0fb2c33545c6e3b6ecfa6695
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-05-22 14:48:29 -0400

    added MapParams iface

---
+++ src/app/services/map/map-params.ts
@@ -0,0 +1,5 @@
+export interface MapParams {
+  lat: number;
+  lng: number;
+  zoom?: number;
+}
--- src/app/services/map/map.service.ts
+++ src/app/services/map/map.service.ts
@@ -2,24 +2,26 @@ import { Injectable, Inject } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { AppConfig, APP_CONFIG } from '../index';
+import { MapParams } from './map-params';
 
 @Injectable()
 export class MapParametersService {
 
-  private subject = new Subject<any>();
+  private subject = new Subject<MapParams>();
 
   constructor(
       @Inject(APP_CONFIG) config: AppConfig
     ){
     console.log('config', config);
+    this.set({lat: config.latitude, lng: config.longitude});
   }
   
-  set(params: any): void {
+  set(params: MapParams): void {
     console.log('MapParametersService.set', params);
     this.subject.next(params);
   }
 
-  get(): Observable<any> {
+  get(): Observable<MapParams> {
     return this.subject.asObservable();
   }
   
