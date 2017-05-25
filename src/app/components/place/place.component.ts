37c347c810e904142d0d4dbff0044cfa672b4552
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-05-25 07:39:34 -0400

    undisciplined commit

--- src/app/components/place/place.component.ts
+++ src/app/components/place/place.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 @Component({
   selector: 'app-place',
   template: `
-    <place-details></place-details>
+    <div fxLayout="column" fxLayoutAlign="stretch">
+      <swipe fxFlex="50"></swipe>
+      <place-details></place-details>
+    </div>
   `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
--- src/app/components/swipe/swipe.component.ts
+++ src/app/components/swipe/swipe.component.ts
@@ -26,7 +26,7 @@ export class SwipeComponent {
   };
   public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
+  public barChartLegend:boolean = false;
  
   public barChartData:any[] = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
