02825be151b777db99264390fe1f44172881ffe3
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-06-01 19:06:13 -0400

    refactor components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,49 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule, BrowserXhr } from '@angular/http';
-import {MaterialModule} from '@angular/material';
+import { MaterialModule} from '@angular/material';
 import { FlexLayoutModule } from "@angular/flex-layout";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AgmCoreModule } from 'angular2-google-maps/core';
-import {environment} from '../environments/environment'
-import { AppRoutingModule } from './app-routing.module';
 import { ChartsModule } from 'ng2-charts';
-import {INIT_CONFIG, APP_CONFIG} from './config';
+import 'hammerjs';
 import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { SwiperModule, SwiperConfigInterface } from 'ngx-swiper-wrapper';
-import { SwipeComponent } from './components/swipe/swipe.component';
-
-import 'hammerjs';
-
-import { AppComponent } from './app.component';
-import { BogusComponent } from './components';
-import { AnotherComponent } from './components/test-component/another.component';
-import { PlaceTypesMenuComponent } from './components';
-import { PlaceTypesSelectComponent } from './components';
-import { DialogComponent } from './components';
-import { MapParametersService, 
-         StorageService,
-         PlaceDetailsResolverService,
-         SelectedPlaceTypeService,
-         WindowRefService, 
-         RandomTypeRedirectionService,
-         GeolocationService,
-         PlaceTypeResolverService } from './services';
-import { GooglePlacesService } from './google-places.service';
-import { HourlyDataService } from './hourly-data.service';
-import { GooglePlacesNearbySearchService } from './services';
-import { GooglePlacesRadarSearchService } from './services';
-import { MapComponent } from './components/map/map.component';
-import { EstablishmentsService } from './establishments.service';
-import { PlaceDetailsComponent } from './components/place-details/place-details.component';
-import { PlaceComponent } from './components/place/place.component';
-import { StarRatingComponent } from './components/star-rating/star-rating.component';
-
-export class MyHammerConfig extends HammerGestureConfig  {
-  overrides = <any>{
-      'swipe': {velocity: 0.4, threshold: 20} // override default settings
-  }
-}
 
 const SWIPER_CONFIG: SwiperConfigInterface = {
   direction: 'horizontal',
@@ -52,6 +17,44 @@ const SWIPER_CONFIG: SwiperConfigInterface = {
   centeredSlides: true,
   keyboardControl: true
 };
+
+export class MyHammerConfig extends HammerGestureConfig  {
+  overrides = <any>{
+      'swipe': {velocity: 0.4, threshold: 20} // override default settings
+  }
+}
+
+
+import { environment} from '../environments/environment'
+import { AppRoutingModule } from './app-routing.module';
+import { INIT_CONFIG, APP_CONFIG} from './config';
+
+import { AppComponent } from './app.component';
+import {BogusComponent,
+        AnotherComponent,
+        SwipeComponent, 
+        MapComponent,
+        PlaceDetailsComponent,
+        PlaceComponent,
+        StarRatingComponent,
+        PlaceTypesMenuComponent,
+        PlaceTypesSelectComponent,
+        DialogComponent } from './components';
+
+import { MapParametersService, 
+         StorageService,
+         PlaceDetailsResolverService,
+         SelectedPlaceTypeService,
+         WindowRefService, 
+         GooglePlacesNearbySearchService,
+         GooglePlacesRadarSearchService, 
+         RandomTypeRedirectionService,
+         GeolocationService,
+         PlaceTypeResolverService } from './services';
+import { GooglePlacesService } from './google-places.service';
+import { HourlyDataService } from './hourly-data.service';
+import { EstablishmentsService } from './establishments.service';
+
 
 @NgModule({
   declarations: [
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,6 +2,12 @@ export * from '../config';
 export * from '../place-types';
 export { DialogComponent } from './dialog/dialog.component';
 export { BogusComponent } from './bogus/bogus.component';
+export { MapComponent } from './map/map.component';
+export { PlaceDetailsComponent } from './place-details/place-details.component';
 export { PlaceTypesMenuComponent } from './place-types-menu/place-types-menu.component';
 export { PlaceTypesSelectComponent } from './place-types-select/place-types-select.component';
+export { PlaceComponent } from './place/place.component';
+export { StarRatingComponent } from './star-rating/star-rating.component';
+export {  SwipeComponent } from './swipe/swipe.component';
+export {  AnotherComponent } from './test-component/another.component';
 
