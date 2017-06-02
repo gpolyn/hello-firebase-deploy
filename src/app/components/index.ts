4b75a82f086cb1758e0a2982d68afa9fec4554fa
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-06-01 20:08:57 -0400

    returned VisualizationComponent to ngModule reference

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -3,6 +3,7 @@ export * from '../place-types';
 export { DialogComponent } from './dialog/dialog.component';
 export { BogusComponent } from './bogus/bogus.component';
 export { MapComponent } from './map/map.component';
+export { VisualizationComponent } from './alt-swipe/visualization.component';
 export { PlaceDetailsComponent } from './place-details/place-details.component';
 export { PlaceTypesMenuComponent } from './place-types-menu/place-types-menu.component';
 export { PlaceTypesSelectComponent } from './place-types-select/place-types-select.component';
