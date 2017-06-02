07def508d4d38ce67e5d339ab6e7546b90e006b7
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-06-02 13:41:28 -0400

    added use of jscript api to GooglePlacesService

--- src/app/services/places/google-places.service.ts
+++ src/app/services/places/google-places.service.ts
@@ -1,23 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { MapsAPILoader } from 'angular2-google-maps/core';
+import { DOCUMENT } from '@angular/platform-browser';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import {environment} from '../index';
+declare var google: any;
 
 @Injectable()
 export class GooglePlacesService {
 
   private apiKey: string;
   private selection: any;
+  private jsPlacesService: any; 
 
-  constructor(private http: Http) {
+  constructor(
+    @Inject(DOCUMENT) private document: any,
+    private mapsAPILoader: MapsAPILoader,
+    private http: Http) {
     this.apiKey = environment.google_maps_api_key;
+    this.mapsAPILoader.load().then(() => {
+      const container = this.document.createElement('div');
+      this.jsPlacesService = new google.maps.places.PlacesService(container);
+    });
   }
 
   getPlaceData(placeId: string): Observable<any> {
+    // return this.webServiceMode(placeId);
+    const request = {placeId: placeId};
+    const prom = new Promise((resolve, reject) => {
+      this.jsPlacesService.getDetails(request, (result) => {
+        resolve(result);
+      });
+    });
+    return Observable.fromPromise(prom);
+  }
+
+  private webServiceMode(placeId: string): Observable<any> {
 		return this.http.get(this.requestString(placeId))
 								.map(this.extractData)
 								.catch(this.handleError);
