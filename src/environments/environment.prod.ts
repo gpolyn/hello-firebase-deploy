639e0592f72dd1e5a8bb1734c7efae52fcd82118
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-06-02 10:11:42 -0400

    added production Google API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  google_maps_api_key: 'AIzaSyAQ-DQlMCEFnG6VompZdKLyK7l5NjI3ojg'
+  google_maps_api_key: 'AIzaSyC9r3_MI9AZM7XucoW-1Z_9iKhAlb4YhT4'
 };
