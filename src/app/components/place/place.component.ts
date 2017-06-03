538e5bce350917b854fee483c221ed9a11de2fb9
Gallagher Polyn <gallagher.polyn@gmail.com>
2017-06-02 21:49:22 -0400

    mod styles to fit all elements

--- src/app/components/place/place.component.ts
+++ src/app/components/place/place.component.ts
@@ -3,10 +3,13 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-place',
+  host: {class: 'myClass'},
   template: `
-    <div style='margin-top: 6px' fxLayout="column" fxLayoutGap="6px" fxLayoutAlign="stretch">
-      <swipe fxFlex="50"></swipe>
-      <place-details></place-details>
+    <!-- <div style='margin-top: 6px' fxLayout="column" fxLayoutGap="6px" fxLayoutAlign="stretch"> -->
+    <div style='margin-top: 6px' fxLayoutGap="6px" fxFlex fxLayout="column">
+      <!-- <swipe fxLayoutAlign="stretch" fxFlex="65%"></swipe> -->
+      <swipe fxLayoutAlign="stretch" fxFlex></swipe>
+      <place-details fxFlex="35%"></place-details>
     </div>
   `,
   styles: [],
